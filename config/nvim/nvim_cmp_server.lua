dd6c546f7cbc1059bd6e097faa5c35cb8a3971a1
burntfalafel <86749075+burntfalafel@users.noreply.github.com>
2022-05-02 11:29:44 +0100

    Update nvim_cmp_server.lua

--- config/nvim/nvim_cmp_server.lua
+++ config/nvim/nvim_cmp_server.lua
@@ -53,7 +53,7 @@ local lspconfig = require('lspconfig')
 
 -- Enable some language servers with the additional completion capabilities offered by nvim-cmp
 -- local servers = { 'clangd', 'rust_analyzer', 'pyright', 'tsserver' }
-local servers = { 'clangd', 'pyright' }
+local servers = { 'clangd', 'pyright', 'cmake', 'sumneko_lua'}
 for _, lsp in ipairs(servers) do
   lspconfig[lsp].setup {
     -- on_attach = my_custom_on_attach,
