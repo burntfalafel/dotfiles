ed0afbaa390a295fa1b9ff032be581c11e96822b
burntfalafel <wadhwae@tcd.ie>
2022-06-25 17:07:51 +0100

    swithced back to using vsnip and added back util for autocompletion

--- config/nvim/nvim_cmp_server.lua
+++ config/nvim/nvim_cmp_server.lua
@@ -5,23 +5,39 @@ cmp.setup({
   snippet = {
     -- REQUIRED - you must specify a snippet engine
     expand = function(args)
-      vim.fn["UltiSnips#Anon"](args.body) -- For `ultisnips` users.
+      vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
     end,
   },
-  mapping = {
-    ['<C-b>'] = cmp.mapping(cmp.mapping.scroll_docs(-4), { 'i', 'c' }),
-    ['<C-f>'] = cmp.mapping(cmp.mapping.scroll_docs(4), { 'i', 'c' }),
-    ['<C-Space>'] = cmp.mapping(cmp.mapping.complete(), { 'i', 'c' }),
-    ['<C-y>'] = cmp.config.disable, -- Specify `cmp.config.disable` if you want to remove the default `<C-y>` mapping.
-    ['<C-e>'] = cmp.mapping({
-      i = cmp.mapping.abort(),
-      c = cmp.mapping.close(),
-    }),
-    ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
-  },
-  sources = cmp.config.sources({
+  mapping = cmp.mapping.preset.insert ({
+   ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+   ['<C-f>'] = cmp.mapping.scroll_docs(4),
+   ['<C-Space>'] = cmp.mapping.complete(),
+   ['<C-e>'] = cmp.mapping.abort(),
+   ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+
+   ["<Tab>"] = cmp.mapping(function(fallback)
+     if cmp.visible() then
+       cmp.select_next_item()
+     elseif vim.fn["vsnip#available"](1) == 1 then
+       feedkey("<Plug>(vsnip-expand-or-jump)", "")
+     elseif has_words_before() then
+       cmp.complete()
+     else
+       fallback() -- The fallback function sends a already mapped key. In this case, it's probably `<Tab>`.
+     end
+   end, { "i", "s" }),
+
+   ['<S-Tab>'] = cmp.mapping(function()
+     if cmp.visible() then
+       cmp.select_prev_item()
+     elseif vim.fn["vsnip#jumpable"](-1) == 1 then
+       feedkey("<Plug>(vsnip-jump-prev)", "")
+     end
+   end, { "i", "s"}),
+ }),
+ sources = cmp.config.sources({
     { name = 'nvim_lsp' },
-    { name = 'ultisnips' }, -- For ultisnips users.
+    { name = 'vsnip' }, -- For vsnip users.
   }, {
     { name = 'buffer' },
   })
