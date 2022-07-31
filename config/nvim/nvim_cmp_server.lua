05527b8f42352a3d5ed24681046571de39181d5d
burntfalafel <wadhwae@tcd.ie>
2022-07-31 19:27:58 +0100

    can now goto, thanks to my big brain

--- config/nvim/nvim_cmp_server.lua
+++ config/nvim/nvim_cmp_server.lua
@@ -1,6 +1,62 @@
 -- Setup nvim-cmp.
 local cmp = require'cmp'
 
+local lsp_symbols = {
+  Text = "   (Text) ",
+  Method = "   (Method)",
+  Function = "   (Function)",
+  Constructor = "   (Constructor)",
+  Field = " ﴲ  (Field)",
+  Variable = "[] (Variable)",
+  Class = "   (Class)",
+  Interface = " ﰮ  (Interface)",
+  Module = "   (Module)",
+  Property = " 襁 (Property)",
+  Unit = "   (Unit)",
+  Value = "   (Value)",
+  Enum = " 練 (Enum)",
+  Keyword = "   (Keyword)",
+  Snippet = "   (Snippet)",
+  Color = "   (Color)",
+  File = "   (File)",
+  Reference = "   (Reference)",
+  Folder = "   (Folder)",
+  EnumMember = "   (EnumMember)",
+  Constant = " ﲀ  (Constant)",
+  Struct = " ﳤ  (Struct)",
+  Event = "   (Event)",
+  Operator = "   (Operator)",
+  TypeParameter = "   (TypeParameter)"
+}
+
+-- used by keybinding_attach
+local map = function(type, key, value)
+	vim.api.nvim_buf_set_keymap(0,type,key,value,{noremap = true, silent = true});
+end
+
+local keybinding_attach = function(client)
+	-- require'completion'.on_attach(client)
+	-- require'diagnostic'.on_attach(client)
+
+	map('n','gD','<cmd>lua vim.lsp.buf.declaration()<CR>')
+	map('n','gd','<cmd>lua vim.lsp.buf.definition()<CR>')
+	map('n','gk','<cmd>lua vim.lsp.buf.hover()<CR>')
+	map('n','gr','<cmd>lua vim.lsp.buf.references()<CR>')
+	map('n','gs','<cmd>lua vim.lsp.buf.signature_help()<CR>')
+	map('n','gi','<cmd>lua vim.lsp.buf.implementation()<CR>')
+	map('n','gt','<cmd>lua vim.lsp.buf.type_definition()<CR>')
+	map('n','<leader>gw','<cmd>lua vim.lsp.buf.document_symbol()<CR>')
+	map('n','<leader>gW','<cmd>lua vim.lsp.buf.workspace_symbol()<CR>')
+	map('n','<leader>ah','<cmd>lua vim.lsp.buf.hover()<CR>')
+	map('n','<leader>af','<cmd>lua vim.lsp.buf.code_action()<CR>')
+	map('n','<leader>ee','<cmd>lua vim.lsp.util.show_line_diagnostics()<CR>')
+	map('n','<leader>ar','<cmd>lua vim.lsp.buf.rename()<CR>')
+	map('n','<leader>=', '<cmd>lua vim.lsp.buf.formatting()<CR>')
+	map('n','<leader>ai','<cmd>lua vim.lsp.buf.incoming_calls()<CR>')
+	map('n','<leader>ao','<cmd>lua vim.lsp.buf.outgoing_calls()<CR>')
+end
+
+
 cmp.setup({
   snippet = {
     -- REQUIRED - you must specify a snippet engine
@@ -13,7 +69,7 @@ cmp.setup({
    ['<C-f>'] = cmp.mapping.scroll_docs(4),
    ['<C-Space>'] = cmp.mapping.complete(),
    ['<C-e>'] = cmp.mapping.abort(),
-   ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+   ['<CR>'] = cmp.mapping.confirm({ select = false }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
 
    ["<Tab>"] = cmp.mapping(function(fallback)
      if cmp.visible() then
@@ -38,6 +94,8 @@ cmp.setup({
  sources = cmp.config.sources({
     { name = 'nvim_lsp' },
     { name = 'vsnip' }, -- For vsnip users.
+    { name = 'path' },
+    { name = 'zsh' },
   }, {
     { name = 'buffer' },
   })
@@ -73,6 +131,7 @@ local servers = { 'clangd', 'pyright', 'cmake', 'sumneko_lua'}
 for _, lsp in ipairs(servers) do
   lspconfig[lsp].setup {
     -- on_attach = my_custom_on_attach,
+    on_attach = keybinding_attach,
     capabilities = capabilities,
   }
 end
