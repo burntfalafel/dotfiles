7da2e0f152a079b0e685c0e930a051825aad9790
burntfalafel <86749075+burntfalafel@users.noreply.github.com>
2022-08-06 10:02:03 +0100

    tab completition bugfix

--- config/nvim/nvim_cmp_server.lua
+++ config/nvim/nvim_cmp_server.lua
@@ -34,6 +34,12 @@ local map = function(type, key, value)
 	vim.api.nvim_buf_set_keymap(0,type,key,value,{noremap = true, silent = true});
 end
 
+-- used by <Tab>
+local function has_words_before()
+	local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+	return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+end
+
 local keybinding_attach = function(client)
 	-- require'completion'.on_attach(client)
 	-- require'diagnostic'.on_attach(client)
